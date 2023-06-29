701503af57fd583bdc6b89226e2e1b6f05c39cff
Alessandro Gilardi <alegil0206@gmail.com>
2023-06-29 15:11:37 +0200

    ulteriore correzione al path

--- dags/utilities/dockerRunner.py
+++ dags/utilities/dockerRunner.py
@@ -2,7 +2,7 @@ import docker
 from utilities.customException import DockerApiException, ArcanImageNotFoundException, ArcanExecutionException, DockerException
 
 def execute_analysis(version: int, project_language:str, arcan_image: str, dependency_graph_name: str):
-    cmd = f'4h /arcan-cli/arcan.sh analyse-graph --graphFile /projects/dependency-graph/arcanOutput/{dependency_graph_name} --versionId {version["id_github"]} -i /projects/{version["id"]} -o /projects/analysis -l {project_language} --vcs NO_VCS --all output.writeDependencyGraph=true output.writeSmellCharacteristics=false output.writeComponentMetrics=false output.writeAffected=false output.writeProjectMetrics=false'
+    cmd = f'4h /arcan-cli/arcan.sh analyse-graph --graphFile /projects/dependency-graph/arcanOutput/{version["id"]}/{dependency_graph_name} --versionId {version["id_github"]} -i /projects/{version["id"]} -o /projects/analysis -l {project_language} --vcs NO_VCS --all output.writeDependencyGraph=true output.writeSmellCharacteristics=false output.writeComponentMetrics=false output.writeAffected=false output.writeProjectMetrics=false'
     execute_container(cmd, arcan_image, version["id"])
 
 def execute_parsing(version_id: int, project_language:str, arcan_image: str):
