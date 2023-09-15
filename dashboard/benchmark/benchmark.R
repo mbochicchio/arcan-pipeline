12607715e7ce856c8cc12b3900a2e60140fd1437
Darius Sas <darius.sas@outlook.com>
2023-09-15 09:18:50 +0200

    Fixed column names and type and some edge cases

--- dashboard/benchmark/benchmark.R
+++ dashboard/benchmark/benchmark.R
@@ -21,30 +21,37 @@ get_connection_from_env <- function() {
 }
 
 fetch_analyses_page <- function(conn, offset = 0, page_size = 5) {
-    q_page <- '
-        SELECT a.id, a.date_analysis, a.file_result as file_result_id, p.name, p.language,
+    q_page <- "
+        SELECT a.id as analysis_id, a.date_analysis,
+            a.file_result as file_result_id,
+            p.id as project_id, p.name, p.language,
             v.id_github as version, v.date as version_date
-        FROM Analysis as a 
-            JOIN Version as v on v.id = a.project_version 
-            JOIN Project as p on p.id = id_project 
-        WHERE status = \'SUCCESSFUL\'
-        ORDER BY a.date_analysis, a.id ASC'
+        FROM Analysis as a
+            JOIN Version as v on v.id = a.project_version
+            JOIN Project as p on p.id = id_project
+        WHERE a.status = 'SUCCESSFUL'
+        ORDER BY a.date_analysis, a.id ASC"
     q_page <- paste0(q_page, " LIMIT ", page_size, " OFFSET ", offset)
     page <- dbGetQuery(conn, q_page) %>% as_tibble()
 
+    if (nrow(page) == 0) {
+        return(tibble())
+    }
+    
     q_data <- "SELECT id, HEX(file_result) FROM AnalysisResult WHERE id in "
     q_data <- paste0(q_data, "(", paste0(page$file_result_id, collapse = ","), ")")
 
     graphs <- dbGetQuery(conn, q_data) %>% as_tibble()
     graphs %>%
+        rename(graph_id = id) %>%
         rename(file_result = `HEX(file_result)`) %>%
-        left_join(page %>% select(!id), by = c("id" = "file_result_id")) %>%
+        left_join(page, by = c("graph_id" = "file_result_id")) %>%
         select(!file_result, file_result)
 }
 
 hex_to_raw <- function(x) {
-  chars <- strsplit(x, "")[[1]]
-  as.raw(strtoi(paste0(chars[c(TRUE, FALSE)], chars[c(FALSE, TRUE)]), base=16L))
+    chars <- strsplit(x, "")[[1]]
+    as.raw(strtoi(paste0(chars[c(TRUE, FALSE)], chars[c(FALSE, TRUE)]), base = 16L))
 }
 write_hex_to_file <- function(file_name, hex_data) {
     gz <- file(file_name, "wb")
@@ -53,68 +60,74 @@ write_hex_to_file <- function(file_name, hex_data) {
 }
 
 new_data_generator <-
-    generator(function(conn, max_projects = -1, page_size = 5) {
-    i <- 0
+    generator(function(conn, max_analyses = -1, page_size = 5, offset = 0) {
+        i <- offset
 
-    pb <- txtProgressBar(title = "Projects")
-    repeat {
-        page_data <-
-            fetch_analyses_page(conn, offset = i, page_size = page_size)
-        n_page <- nrow(page_data)
-        if (n_page <= 0 || (max_projects > 0 && i > max_projects)) {
-            break
+        repeat {
+            cat(paste("Saving project", i))
+            page_data <-
+                fetch_analyses_page(conn, offset = i, page_size = page_size)
+            n_page <- nrow(page_data)
+            if (n_page <= 0 || (max_analyses > 0 && i > max_analyses)) {
+                break
+            }
+            i <- i + n_page
+            tmp_file <- "/tmp/graph.graphml.gz"
+            graph_file <- "/tmp/graph.graphml"
+            for (index in seq_len(n_page)) {
+                analysis <- page_data[index, ]
+
+                write_hex_to_file(tmp_file, analysis$file_result)
+                gunzip(tmp_file, remove = TRUE, overwrite = TRUE)
+                G <- read_graph(graph_file, "graphml")
+                V(G)$qualified_name <- V(G)$name
+                V(G)$name <- V(G)$id
+                df_nodes <- as_data_frame(G, what = "vertices") %>% as_tibble()
+                df_edges <- as_data_frame(G, what = "edges") %>% as_tibble()
+
+                df_nodes$project_id <- analysis$project_id
+                df_nodes$project_analysis_id <- analysis$analysis_id
+                df_nodes$project_analysis_date <- as.character(analysis$date_analysis)
+                df_nodes$project_name <- analysis$name
+                df_nodes$project_language <- analysis$language
+                df_nodes$project_version <- analysis$version
+                df_nodes$project_version_date <- as.character(analysis$version_date)
+
+                df_edges$project_id <- analysis$project_id
+                df_edges$project_analysis_id <- analysis$analysis_id
+                df_edges$project_analysis_date <- as.character(analysis$date_analysis)
+                df_edges$project_name <- analysis$name
+                df_edges$project_language <- analysis$language
+                df_edges$project_version <- analysis$version
+                df_edges$project_version_date <- as.character(analysis$version_date)
+                df_edges$index <- NULL
+
+                df_nodes <- df_nodes %>%
+                    mutate_if(is.character, as.factor) %>%
+                    mutate_if(is.numeric, \(x) replace_na(x, 0))
+
+                df_edges <- df_edges %>%
+                    mutate_if(is.character, as.factor) %>%
+                    mutate_if(is.numeric, \(x) replace_na(x, 0))
+                yield(list(nodes = df_nodes, edges = df_edges))
+            }
+            suppressWarnings({
+                file.remove(tmp_file)
+            })
         }
-        i <- i + n_page
-        tmp_file <- "/tmp/graph.graphml.gz"
-        graph_file <- "/tmp/graph.graphml"
-        for (index in seq_len(n_page)){
-            analysis <- page_data[index, ]
-
-            write_hex_to_file(tmp_file, analysis$file_result)
-            gunzip(tmp_file, remove = TRUE, overwrite = TRUE)
-            G <- read_graph(graph_file, "graphml")
-            V(G)$qualified_name <- V(G)$name
-            V(G)$name <- V(G)$id
-            df_nodes <- as_data_frame(G, what = "vertices") %>% as_tibble()
-            df_edges <- as_data_frame(G, what = "edges") %>% as_tibble()
-
-            df_nodes$project_id <- analysis$id
-            df_nodes$project_analysis_data <- analysis$date_analysis
-            df_nodes$project_name <- analysis$name
-            df_nodes$project_language <- analysis$language
-            df_nodes$project_version <- analysis$version
-            df_nodes$project_version_date <- as.Date(analysis$version_date)
-
-            df_edges$project_id <- analysis$id
-            df_edges$project_analysis_data <- analysis$date_analysis
-            df_edges$project_name <- analysis$name
-            df_edges$project_language <- analysis$language
-            df_edges$project_version <- analysis$version
-            df_edges$project_version_date <- as.Date(analysis$version_date)
-            df_edges$index <- NULL
-
-            df_nodes <- df_nodes %>%
-                mutate_if(is.character, as.factor) %>%
-                mutate_if(is.numeric, \(x) replace_na(x, 0))
-
-            df_edges <- df_edges %>%
-                mutate_if(is.character, as.factor) %>%
-                mutate_if(is.numeric, \(x) replace_na(x, 0))
-            setTxtProgressBar(pb, 1 / max_projects)
-            yield(list(nodes = df_nodes, edges = df_edges))
-        }
-        close(pb)
-        suppressWarnings({ file.remove(tmp_file) })
-    }
-})
+    })
 
 save_to_sqlite <- function(db_file_name, data_generator) {
     mydb <- dbConnect(RSQLite::SQLite(), db_file_name)
 
-    types_map <- list("factor" = "TEXT", "integer" = "REAL",
-        "numeric" = "REAL", "character" = "TEXT", "logical" = "INTEGER")
-    defaults_map <- list("factor" = "''", "integer" = "0",
-        "numeric" = "0", "character" = "''", "logical" = "FALSE")
+    types_map <- list(
+        "factor" = "TEXT", "integer" = "REAL",
+        "numeric" = "REAL", "character" = "TEXT", "logical" = "INTEGER"
+    )
+    defaults_map <- list(
+        "factor" = "''", "integer" = "0",
+        "numeric" = "0", "character" = "''", "logical" = "FALSE"
+    )
 
     while (!is_exhausted(data <- data_generator())) {
         nodes_info <- dbGetQuery(mydb, "PRAGMA table_xinfo(nodes);")
@@ -122,11 +135,13 @@ save_to_sqlite <- function(db_file_name, data_generator) {
             missing_cols <- setdiff(colnames(data$nodes), nodes_info$name)
             for (col in missing_cols) {
                 col_type <- class(data$nodes[[col]])
-                query <- paste(sep = " ",
+                query <- paste(
+                    sep = " ",
                     "ALTER TABLE nodes", "ADD", col,
                     types_map[col_type],
                     "NOT NULL", "CONSTRAINT", paste0("D_nodes_", col),
-                    "DEFAULT", "(", defaults_map[col_type], ")", ";")
+                    "DEFAULT", "(", defaults_map[col_type], ")", ";"
+                )
                 dbExecute(mydb, query)
             }
         }
@@ -136,11 +151,13 @@ save_to_sqlite <- function(db_file_name, data_generator) {
             missing_cols <- setdiff(colnames(data$edges), edges_info$name)
             for (col in missing_cols) {
                 col_type <- class(data$edges[[col]])
-                query <- paste(sep = " ",
+                query <- paste(
+                    sep = " ",
                     "ALTER TABLE edges", "ADD", col,
                     types_map[col_type],
                     "NOT NULL", "CONSTRAINT", paste0("D_edges_", col),
-                    "DEFAULT", "(", defaults_map[col_type], ")", ";")
+                    "DEFAULT", "(", defaults_map[col_type], ")", ";"
+                )
                 dbExecute(mydb, query)
             }
         }
@@ -152,17 +169,16 @@ save_to_sqlite <- function(db_file_name, data_generator) {
 }
 
 args <- commandArgs(trailingOnly = TRUE)
-if (length(args) < 2) {
-    stop("usage: <output_sqlite> <max_versions>")
+if (length(args) < 3) {
+    stop("usage: <output_sqlite> <max_analyses>")
 }
 output_file <- args[1]
-max_versions <- as.numeric(args[2])
+max_analyses <- as.numeric(args[2])
 conn <- get_connection_from_env()
-tryCatch({
-    fetch_data <- new_data_generator(conn, max_projects = max_versions)
-    save_to_sqlite(output_file, fetch_data)
-}, finally = \(x) dbDisconnect(conn))
-
-
-
-# writeLines(graphs$file_result[1], "/tmp/graph.graphml")
+tryCatch(
+    {
+        fetch_data <- new_data_generator(conn, max_analyses = max_analyses)
+        save_to_sqlite(output_file, fetch_data)
+    },
+    finally = \(x) dbDisconnect(conn)
+)
