8e7c55f0214bf340447901f84cb45ade2c8e2542
Matteo Bochicchio <38531443+mbochicchio@users.noreply.github.com>
2025-06-10 20:22:37 +0200

    Update settings.sql to initialize execution_range and correct inception_range value

--- init/settings.sql
+++ init/settings.sql
@@ -1,3 +1,4 @@
 -- This script initializes the settings table with default values.
 INSERT INTO Settings (name, value) VALUES ('inception_range', '10');
 INSERT INTO Settings (name, value) VALUES ('first_index_inception', '1');
+INSERT INTO Settings (name, value) VALUES ('execution_range', '1');
