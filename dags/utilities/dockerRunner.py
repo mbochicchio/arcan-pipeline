741ad530e9b2c5f8566a35714febe2e649c1f282
Matteo Bochicchio <38531443+mbochicchio@users.noreply.github.com>
2025-06-23 00:49:44 +0200

    FIX container execution with timeout monitoring and fix command syntax in dockerRunner.py

--- dags/utilities/dockerRunner.py
+++ dags/utilities/dockerRunner.py
@@ -1,13 +1,14 @@
-import docker
+import docker # type: ignore
 import datetime
+import time
 from utilities.customException import DockerApiException, ArcanImageNotFoundException, ArcanExecutionException, DockerException, MaximumExecutionTimeExeededException
 
 def execute_analysis(version: int, project_language:str, arcan_image: str, dependency_graph: str):
-    cmd = f'4h /arcan-cli/arcan.sh analyse-graph --graphFile {dependency_graph} --versionId {version["id_github"]} -i /projects/{version["id"]} -o /projects/analysis -l {project_language} --vcs NO_VCS --all output.writeDependencyGraph=true output.writeSmellCharacteristics=false output.writeComponentMetrics=false output.writeAffected=false output.writeProjectMetrics=false'
+    cmd = f'analyse-graph --graphFile {dependency_graph} --versionId {version["id_github"]} -i /projects/{version["id"]} -o /projects/analysis -l {project_language} --vcs NO_VCS --all output.writeDependencyGraph=true output.writeSmellCharacteristics=false output.writeComponentMetrics=false output.writeAffected=false output.writeProjectMetrics=false'
     execute_container(cmd, arcan_image, version["id"])
 
 def execute_parsing(version_id: int, project_language:str, arcan_image: str):
-    cmd = f'4h /arcan-cli/arcan.sh analyse -i /projects/{version_id} -o /projects/dependency-graph -l {project_language} --vcs NO_VCS metrics.componentMetrics="none" metrics.smellCharacteristics="none" metrics.projectMetrics="none" metrics.indexCalculators="none" output.writeDependencyGraph=true output.writeSmellCharacteristics=false output.writeComponentMetrics=false output.writeAffected=false output.writeProjectMetrics=false'
+    cmd = f'analyse -i /projects/{version_id} -o /projects/dependency-graph -l {project_language} --vcs NO_VCS metrics.componentMetrics="none" metrics.smellCharacteristics="none" metrics.projectMetrics="none" metrics.indexCalculators="none" output.writeDependencyGraph=true output.writeSmellCharacteristics=false output.writeComponentMetrics=false output.writeAffected=false output.writeProjectMetrics=false'
     execute_container(cmd, arcan_image, version_id)
 
 def execute_container(cmd: str, arcan_image: str, version_id: int):
@@ -15,7 +16,25 @@ def execute_container(cmd: str, arcan_image: str, version_id: int):
     client = docker.from_env()
     try:
         container_name = f'arcan_container_{version_id}'
-        client.containers.run(image=arcan_image, command=cmd, user=50000, name=container_name, entrypoint="timeout", volumes={'arcan-pipeline_shared-volume': {'bind': '/projects', 'mode': 'rw'}}, detach=False, mem_limit='4g', environment=["JAVA_MEMORY=4G"])
+        container = client.containers.run(
+            image=arcan_image,
+            command=cmd,
+            user=50000,
+            name=container_name,
+            volumes={'arcan-pipeline_shared-volume': {'bind': '/projects', 'mode': 'rw'}},
+            detach=True,
+            mem_limit='4g',
+            environment=["JAVA_MEMORY=4G"]
+        )
+
+        # Monitor container execution
+        while container.status != 'exited':
+            time.sleep(1)
+            container.reload()
+            if time.time() - start_time.timestamp() > 14400:  # 4 hours timeout
+                container.kill()
+                raise MaximumExecutionTimeExeededException("Arcan Analysis Maximum Execution Time Exceeded")
+            
     except docker.errors.APIError as e:
         raise DockerApiException("Docker API Exception:", e)
     except docker.errors.ContainerError as e:
